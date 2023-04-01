03a87e3413985c90c426ce96bcb0434e9d7a6c81
Trần Anh <79416608+trananh10022001@users.noreply.github.com>
2023-04-02 01:54:38 +0700

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-exec(open('Amass.py').read())
-os.system('python Amass.py')
-exec(open('DNSPython.py').read())
-os.system('python DNSPython.py')
-exec(open('Nmap,Nuclei.py').read())
-os.system('python Nmap,Nuclei.py')
