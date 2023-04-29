e453358b0d2ec119a2d46acfb078a10a7cc6356a
Trần Anh <79416608+trananh10022001@users.noreply.github.com>
2023-04-29 14:37:07 +0700

    Update Socket.py

--- Socket.py
+++ Socket.py
@@ -8,17 +8,19 @@ async def scan(websocket):
     name = await websocket.recv()
     main.scanDomain(name)
     cursor = DB_Connection.cursor
-    query0 = "SELECT COUNT(*) FROM asm.domain WHERE domain.domain_name = '"+str(name)+"' ORDER BY domain.id DESC LIMIT 1;"
+    query0 = "SELECT domain.id FROM asm.domain WHERE domain.domain_name = '"+str(name)+"' ORDER BY domain.id DESC LIMIT 1;"
     data = cursor.execute(query0)
     for i in cursor.fetchone():
         id_domain = i
-    query1 = "UPDATE asm.domain SET status = 0 WHERE id = "+str(id_domain)
+    print(id_domain)
+    query1 = "UPDATE asm.domain SET domain.status = 0 WHERE domain.id = '"+str(id_domain)+"'"
     cursor.execute(query1)
     now = datetime.now()
-    query2 = "UPDATE asm.domain SET end_time = '"+str(now)+"' WHERE id = "+str(id_domain)
+    query2 = "UPDATE asm.domain SET domain.end_time = '"+str(now)+"' WHERE domain.id = '"+str(id_domain)+"'"
     cursor.execute(query2)
     DB_Connection.connection.commit()
     await websocket.send("true")
+    print('Success')
     
 
 start_server = websockets.serve(scan, "localhost", 1337)
